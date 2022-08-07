1c82e6d51d251f94d09bdeb1043fa97818d9f0a4
natnael-OQP <makiya112545@gmail.com>
2022-08-07 22:31:08 +0300

    add bottomNavigationBar and create variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,24 +20,36 @@ class MyApp extends StatelessWidget {
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
-
   @override
   State<RootPage> createState() => _RootPageState();
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: const HomePage(),
       appBar: AppBar(
         title: const Text("Home"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
-          debugPrint("take a photo") 
+          debugPrint("take nathan")
         },
         backgroundColor: Colors.blue[500],
         child: const Icon(Icons.add_a_photo_outlined),
+      ),
+      bottomNavigationBar: NavigationBar(destinations: const [
+        NavigationDestination(icon: Icon(Icons.home_filled), label: "Home"),
+        NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+      ],
+      onDestinationSelected: (i){
+        setState(() {
+          currentPage=i;
+        });
+      },
+      selectedIndex: currentPage,
       ),
     );
   }
